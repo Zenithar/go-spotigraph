13844984d5f1b32f505a93f575ff58621213d896
Thibault Normand <me@zenithar.org>
2019-03-19 14:43:53 +0100

    chore(go): Add version ,revision and go version in buildinfo.

--- cmd/spotigraph/internal/cmd/version.go
+++ cmd/spotigraph/internal/cmd/version.go
@@ -14,6 +14,6 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Display service version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(version.Version)
+		fmt.Printf("%s-%s (%s)", version.Version, version.Revision, version.GoVersion)
 	},
 }
